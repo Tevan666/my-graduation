03b75d791ff127a8ea6f143365357e264f523421
tevan <1759377257@qq.com>
2021-12-27 16:29:25 +0800

    初始化

---
+++ config/defaultSettings.js
@@ -0,0 +1,15 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '目标检测',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+export default Settings;
