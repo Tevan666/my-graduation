07b98c38a2bf252df12fad03e6c156390905f51a
tevan <1759377257@qq.com>
2021-12-28 19:54:59 +0800

    添加上传功能

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
 import { Card, Typography, Alert } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { useIntl } from 'umi';
+import { Upload, Modal } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 
 const Admin = () => {
   const intl = useIntl();
+
+  function getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
+  const [state, setState] = useState({
+    previewVisible: false,
+    previewImage: '',
+    previewTitle: '',
+    fileList: [],
+  });
+
+  const handleCancel = () => setState({ previewVisible: false });
+
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+
+    setState({
+      previewImage: file.url || file.preview,
+      previewVisible: true,
+      previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+    });
+  };
+
+  const handleChange = ({ fileList }) => setState({ fileList });
+  const { previewVisible, previewImage, fileList, previewTitle } = state;
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
   return (
     <PageHeaderWrapper
       content={intl.formatMessage({
@@ -16,8 +56,8 @@ const Admin = () => {
       <Card>
         <Alert
           message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
+            id: 'pages.welcome.animal',
+            defaultMessage: '识别近八千种动物',
           })}
           type="success"
           showIcon
@@ -27,6 +67,15 @@ const Admin = () => {
             marginBottom: 48,
           }}
         />
+        <Upload
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={handlePreview}
+          onChange={handleChange}
+        >
+          {fileList.length >= 8 ? null : uploadButton}
+        </Upload>
         <Typography.Title
           level={2}
           style={{
@@ -35,19 +84,10 @@ const Admin = () => {
         >
           <SmileTwoTone /> Ant Design Pro <HeartTwoTone twoToneColor="#eb2f96" /> You
         </Typography.Title>
+        <Modal visible={previewVisible} title={previewTitle} footer={null} onCancel={handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
       </Card>
-      <p
-        style={{
-          textAlign: 'center',
-          marginTop: 24,
-        }}
-      >
-        Want to add more pages? Please refer to{' '}
-        <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-          use block
-        </a>
-        。
-      </p>
     </PageHeaderWrapper>
   );
 };
