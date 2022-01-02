b6a9f2edc4934b14be5761d00104547b7fbe017f
Tevan <1759377257@qq.com>
2022-01-03 01:50:47 +0800

    解决折线图y轴大小显示问题

--- src/pages/Categories/line.jsx
+++ src/pages/Categories/line.jsx
@@ -1,25 +1,32 @@
 import { Line } from '@ant-design/charts';
-import { useState } from 'react';
 
 const Page = (lineData) => {
-  console.log(lineData);
   const res = lineData.lineData;
-  console.log(res);
   const resList = [];
   res.map((item) => {
+    item.score = (item.score * 100) / 100;
     resList.push(item);
   });
-  console.log(resList, 'list');
-  const data = resList;
+  const data = res;
 
   const config = {
     data,
     xField: 'name',
     yField: 'score',
+    yAxis: {
+      title: {
+        text: '分类结果',
+        position: 'end',
+      },
+      label: {
+        formatter: (value) => (value * 100) / 100,
+      },
+    },
     point: {
       size: 5,
       shape: 'diamond',
     },
+    appendPadding: [16, 8, 16, 8],
     lineStyle: {
       stroke: 'black',
       lineWidth: 2,
