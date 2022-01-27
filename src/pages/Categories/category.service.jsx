a4a2965f0568659d95f5a5c5385e0ebf4979e150
tevan <1759377257@qq.com>
2022-01-27 15:22:31 +0800

    更换token

--- src/pages/Categories/category.service.jsx
+++ src/pages/Categories/category.service.jsx
@@ -3,7 +3,7 @@ import request, { extend } from 'umi-request';
 //封装接口，image为解码后的图片，num为百科条数，classifiy为类别
 export async function getResults(image, num, classify) {
   return request(
-    `/rest/2.0/image-classify/v1/${classify}?access_token=24.ba42db818351143b0b0808785d427fa8.2592000.1643275108.282335-25425850`,
+    `/rest/2.0/image-classify/v1/${classify}?access_token=24.24.4688ae0d7f8e813cc6f6f32871d7febd.2592000.1645088626.282335-25425850`,
     {
       method: 'POST',
       headers: { 'Content-type': 'application/x-www-form-urlencoded' },
