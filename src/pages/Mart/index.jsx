dcab973b836ceaa4a3b721791ccaf12259de7c73
Tevan <1759377257@qq.com>
2022-02-18 00:21:53 +0800

    商品页动物识别细节优化

--- src/pages/Mart/index.jsx
+++ src/pages/Mart/index.jsx
@@ -1,13 +1,15 @@
-import ProCard from '@ant-design/pro-card';
+import { EllipsisOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import ProCard, { StatisticCard } from '@ant-design/pro-card';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Select, Space } from 'antd';
+import { Button, Select, Space, Statistic } from 'antd';
 import { useState } from 'react';
+import RcResizeObserver from 'rc-resize-observer';
 
 const { Option } = Select;
 
 const Mart = () => {
   const [tabPosition, setTabPosition] = useState('left');
-
+  const [responsive, setResponsive] = useState(false);
   const [tab, setTab] = useState('1');
   return (
     <>
@@ -35,14 +37,79 @@ const Mart = () => {
           }}
         >
           <ProCard.TabPane key="1" tab="动物识别">
-            <ProCard headerBordered direction="column">
-              <ProCard bordered title="资源管理">
-                qqq
+            <RcResizeObserver
+              key="resize-observer"
+              onResize={(offset) => {
+                setResponsive(offset.width < 596);
+              }}
+            >
+              <ProCard
+                title="数据概览"
+                extra="2019年9月28日 星期五"
+                split={responsive ? 'horizontal' : 'vertical'}
+                headerBordered
+                bordered
+              >
+                <ProCard split="horizontal">
+                  <ProCard split="horizontal">
+                    <ProCard split="vertical">
+                      <StatisticCard
+                        statistic={{
+                          title: '昨日购买量',
+                          value: 234,
+                          description: (
+                            <Statistic title="较本月平均购买量" value="8.04%" trend="down" />
+                          ),
+                        }}
+                      />
+                      <StatisticCard
+                        statistic={{
+                          title: '本月累计购买量',
+                          value: 234,
+                          description: <Statistic title="月同比" value="8.04%" trend="up" />,
+                        }}
+                      />
+                    </ProCard>
+                    <ProCard split="vertical">
+                      <StatisticCard
+                        title={
+                          <Button size="large" type="dashed" className="flex align-middle">
+                            限时免费领取
+                            <PlusCircleOutlined />
+                          </Button>
+                        }
+                      />
+                      <StatisticCard
+                        statistic={{
+                          title: '历史购买总数',
+                          value: '134',
+                          suffix: '个',
+                        }}
+                      />
+                    </ProCard>
+                  </ProCard>
+                  <StatisticCard
+                    title="数量走势"
+                    chart={
+                      <img
+                        src="https://gw.alipayobjects.com/zos/alicdn/_dZIob2NB/zhuzhuangtu.svg"
+                        width="100%"
+                      />
+                    }
+                  />
+                </ProCard>
+                <StatisticCard
+                  title="使用时间分布"
+                  chart={
+                    <img
+                      src="https://gw.alipayobjects.com/zos/alicdn/qoYmFMxWY/jieping2021-03-29%252520xiawu4.32.34.png"
+                      alt="大盘"
+                      width="100%"
+                    />
+                  }
+                />
               </ProCard>
-              <ProCard bordered title="开发者资源" style={{ marginTop: 20 }}>
-                资源
-              </ProCard>
-            </ProCard>
+            </RcResizeObserver>
           </ProCard.TabPane>
           <ProCard.TabPane key="2" tab="植物识别">
             <ProCard direction="column">
