c6415003338c8176e153b6ac25eda9b6f3c94372
Tevan <1759377257@qq.com>
2022-02-19 14:52:42 +0800

    商店页抽离组件

---
+++ src/pages/Mart/compoment/TopicMart.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+
+import RcResizeObserver from 'rc-resize-observer';
+import ProCard, { StatisticCard } from '@ant-design/pro-card';
+import { Button, Statistic, Popconfirm, message } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import moment from 'moment';
+
+const handleConfirm = () => {
+  message.success('不给');
+};
+
+const handleCancel = () => {
+  message.error('呵呵');
+};
+
+const TopicMart = (params) => {
+  const [responsive, setResponsive] = useState(false);
+
+  const [time, setTime] = useState(moment().format('YYYY-MM-DD, h:mm:ss a'));
+  setInterval(() => {
+    setTime(moment().format('YYYY-MM-DD, h:mm:ss a'));
+  }, 1000);
+  return (
+    <>
+      <RcResizeObserver
+        key="resize-observer"
+        onResize={(offset) => {
+          setResponsive(offset.width < 596);
+        }}
+      >
+        <ProCard
+          title="数据概览"
+          extra={`${time}`}
+          split={responsive ? 'horizontal' : 'vertical'}
+          headerBordered
+          bordered
+        >
+          <ProCard split="horizontal">
+            <ProCard split="horizontal">
+              <ProCard split="vertical">
+                <StatisticCard
+                  statistic={{
+                    title: '昨日购买量',
+                    value: 234,
+                    description: <Statistic title="较本月平均购买量" value="8.04%" trend="down" />,
+                  }}
+                />
+                <StatisticCard
+                  statistic={{
+                    title: '本月累计购买量',
+                    value: 234,
+                    description: <Statistic title="月同比" value="8.04%" trend="up" />,
+                  }}
+                />
+              </ProCard>
+              <ProCard split="vertical">
+                <StatisticCard
+                  title={
+                    <Popconfirm
+                      title="限时免费领取啦 先到先得！！！"
+                      onConfirm={handleConfirm}
+                      onCancel={handleCancel}
+                      okText="炫就完事了"
+                      cancelText="算了我不要了"
+                    >
+                      <Button size="large" type="dashed" className="flex align-middle">
+                        限时免费领取
+                        <PlusCircleOutlined />
+                      </Button>
+                    </Popconfirm>
+                  }
+                />
+                <StatisticCard
+                  statistic={{
+                    title: '历史购买总数',
+                    value: '134',
+                    suffix: '个',
+                  }}
+                />
+              </ProCard>
+            </ProCard>
+            <StatisticCard
+              title="数量走势"
+              chart={
+                <img
+                  src="https://gw.alipayobjects.com/zos/alicdn/_dZIob2NB/zhuzhuangtu.svg"
+                  width="100%"
+                />
+              }
+            />
+          </ProCard>
+          <StatisticCard
+            title="使用时间分布"
+            chart={
+              <img
+                src="https://gw.alipayobjects.com/zos/alicdn/qoYmFMxWY/jieping2021-03-29%252520xiawu4.32.34.png"
+                alt="大盘"
+                width="100%"
+              />
+            }
+          />
+        </ProCard>
+      </RcResizeObserver>
+    </>
+  );
+};
+
+export default TopicMart;
--- src/pages/Mart/index.jsx
+++ src/pages/Mart/index.jsx
@@ -1,16 +1,14 @@
-import { EllipsisOutlined, PlusCircleOutlined } from '@ant-design/icons';
-import ProCard, { StatisticCard } from '@ant-design/pro-card';
+import ProCard from '@ant-design/pro-card';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Button, Select, Space, Statistic } from 'antd';
+import { Button, Select, Space } from 'antd';
 import { useState } from 'react';
-import RcResizeObserver from 'rc-resize-observer';
-
+import TopicMart from './compoment/TopicMart';
 const { Option } = Select;
 
 const Mart = () => {
   const [tabPosition, setTabPosition] = useState('left');
-  const [responsive, setResponsive] = useState(false);
   const [tab, setTab] = useState('1');
+
   return (
     <>
       <PageContainer>
@@ -37,87 +35,10 @@ const Mart = () => {
           }}
         >
           <ProCard.TabPane key="1" tab="动物识别">
-            <RcResizeObserver
-              key="resize-observer"
-              onResize={(offset) => {
-                setResponsive(offset.width < 596);
-              }}
-            >
-              <ProCard
-                title="数据概览"
-                extra="2019年9月28日 星期五"
-                split={responsive ? 'horizontal' : 'vertical'}
-                headerBordered
-                bordered
-              >
-                <ProCard split="horizontal">
-                  <ProCard split="horizontal">
-                    <ProCard split="vertical">
-                      <StatisticCard
-                        statistic={{
-                          title: '昨日购买量',
-                          value: 234,
-                          description: (
-                            <Statistic title="较本月平均购买量" value="8.04%" trend="down" />
-                          ),
-                        }}
-                      />
-                      <StatisticCard
-                        statistic={{
-                          title: '本月累计购买量',
-                          value: 234,
-                          description: <Statistic title="月同比" value="8.04%" trend="up" />,
-                        }}
-                      />
-                    </ProCard>
-                    <ProCard split="vertical">
-                      <StatisticCard
-                        title={
-                          <Button size="large" type="dashed" className="flex align-middle">
-                            限时免费领取
-                            <PlusCircleOutlined />
-                          </Button>
-                        }
-                      />
-                      <StatisticCard
-                        statistic={{
-                          title: '历史购买总数',
-                          value: '134',
-                          suffix: '个',
-                        }}
-                      />
-                    </ProCard>
-                  </ProCard>
-                  <StatisticCard
-                    title="数量走势"
-                    chart={
-                      <img
-                        src="https://gw.alipayobjects.com/zos/alicdn/_dZIob2NB/zhuzhuangtu.svg"
-                        width="100%"
-                      />
-                    }
-                  />
-                </ProCard>
-                <StatisticCard
-                  title="使用时间分布"
-                  chart={
-                    <img
-                      src="https://gw.alipayobjects.com/zos/alicdn/qoYmFMxWY/jieping2021-03-29%252520xiawu4.32.34.png"
-                      alt="大盘"
-                      width="100%"
-                    />
-                  }
-                />
-              </ProCard>
-            </RcResizeObserver>
+            <TopicMart />
           </ProCard.TabPane>
           <ProCard.TabPane key="2" tab="植物识别">
-            <ProCard direction="column">
-              <ProCard title="操作指引" bordered></ProCard>
-              <ProCard title="资源领取" bordered style={{ marginTop: 20 }}>
-                222
-              </ProCard>
-            </ProCard>
+            <TopicMart />
           </ProCard.TabPane>
         </ProCard>
       </PageContainer>
