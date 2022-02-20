12248679009091a8c32ae167749ba9000ed6c671
Tevan <1759377257@qq.com>
2022-02-20 22:52:46 +0800

    <ProList

--- src/pages/Account/Settings/CommonMessage/index.jsx
+++ src/pages/Account/Settings/CommonMessage/index.jsx
@@ -1,12 +1,43 @@
-import ProList from '@ant-design/pro-list';
+import { useState } from 'react';
 import { useModel } from 'umi';
 import ProDescriptions from '@ant-design/pro-descriptions';
-import Avatar from 'antd/lib/avatar/avatar';
+import { Upload, message, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import ProForm, {
+  ModalForm,
+  ProFormCascader,
+  ProFormText,
+  ProFormTextArea,
+} from '@ant-design/pro-form';
 
+import city from '../city.json';
+
+const props = {
+  name: 'file',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  headers: {
+    authorization: 'authorization-text',
+  },
+  onChange(info) {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+const handleEdit = (values) => {
+  console.log(values);
+  message.success('提交成功');
+};
 const CommonMessage = () => {
   const { initialState, setInitialState } = useModel('@@initialState');
   const { currentUser } = initialState;
-
+  const [editModal, setEditModal] = useState(false);
   return (
     <>
       <ProDescriptions
@@ -14,42 +45,58 @@ const CommonMessage = () => {
           id: '这是一段文本columns',
           date: '20200809',
           money: '1212100',
-          state: 'all',
-          state2: 'open',
+          state: 'closed',
+          square: '广东佛山',
+          description: '我就是我， 不一样的烟火',
         }}
         columns={[
           {
-            title: '文本',
+            title: '头像',
             key: 'text',
-            dataIndex: 'id',
+            dataIndex: `${currentUser.avatar}`,
             ellipsis: true,
             copyable: true,
-            image: `${currentUser.avatar}`,
+            valueType: 'avatar',
+            render: () => {
+              return (
+                <>
+                  <div className="flex items-center">
+                    <img className="max-h-20" src={`${currentUser.avatar}`} alt="" />
+                    <div className="flex flex-col ml-5">
+                      <p>欢迎回来 {currentUser.name}</p>
+                      <Upload {...props}>
+                        <Button icon={<UploadOutlined />}>点击更换头像</Button>
+                      </Upload>
+                    </div>
+                  </div>
+                </>
+              );
+            },
           },
           {
-            title: '状态',
+            title: '账号状态',
             key: 'state',
             dataIndex: 'state',
             valueType: 'select',
             valueEnum: {
               all: { text: '全部', status: 'Default' },
               open: {
-                text: '未解决',
+                text: '异常',
                 status: 'Error',
               },
               closed: {
-                text: '已解决',
+                text: '正常',
                 status: 'Success',
               },
             },
           },
           {
-            title: '状态2',
-            key: 'state2',
-            dataIndex: 'state2',
+            title: '所在地区',
+            key: 'square',
+            dataIndex: 'square',
           },
           {
-            title: '时间',
+            title: '注册时间',
             key: 'date',
             dataIndex: 'date',
             valueType: 'date',
@@ -64,19 +111,46 @@ const CommonMessage = () => {
             title: '操作',
             valueType: 'option',
             render: () => [
-              <a target="_blank" rel="noopener noreferrer" key="link">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                key="link"
+                onClick={() => setEditModal(true)}
+              >
                 编辑
               </a>,
             ],
           },
+          {
+            title: '自我介绍',
+            valueType: 'textarea',
+            key: 'description',
+            dataIndex: 'description',
+          },
         ]}
+      />
+      <ModalForm
+        title="编辑个人资料"
+        visible={editModal}
+        onVisibleChange={setEditModal}
+        onFinish={handleEdit}
+        destroyonclose="true"
       >
-        <ProDescriptions.Item
-          label={<Avatar icon={<img style={{}} src={`${currentUser.avatar}`} />} />}
-        >
-          Hello,{currentUser.name}
-        </ProDescriptions.Item>
-      </ProDescriptions>
+        <ProForm.Group label="用户名">
+          <ProFormText width="md" name="name" tooltip="最长为 10 位" placeholder="请输入名称" />
+        </ProForm.Group>
+        <ProForm.Group label="地区">
+          <ProFormCascader
+            name="square"
+            fieldProps={{
+              options: city,
+            }}
+          />
+        </ProForm.Group>
+        <ProForm.Group label="自我介绍">
+          <ProFormTextArea width="md" name="description" />
+        </ProForm.Group>
+      </ModalForm>
     </>
   );
 };
--- src/pages/Account/Settings/index.jsx
+++ src/pages/Account/Settings/index.jsx
@@ -1,8 +1,59 @@
+import { useState } from 'react';
+
 import ProCard from '@ant-design/pro-card';
 import { PageContainer } from '@ant-design/pro-layout';
+import { Alert, Button, message, Modal, Tag, Space } from 'antd';
+import ProList from '@ant-design/pro-list';
+
 import CommonMessage from './CommonMessage';
 
+const handleDelete = () => {
+  Modal.confirm({
+    title: '你确定?',
+    content: '确定要注销吗',
+    onOk() {
+      message.success('删除成功');
+    },
+    onCancel() {
+      message.warning('爱你么么哒');
+    },
+  });
+};
+
+const defaultData = [
+  {
+    id: '1',
+    name: '语雀的天空',
+    image:
+      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+    desc: '我是一条测试的描述',
+  },
+  {
+    id: '2',
+    name: 'Ant Design',
+    image:
+      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+    desc: '我是一条测试的描述',
+  },
+  {
+    id: '3',
+    name: '蚂蚁金服体验科技',
+    image:
+      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+    desc: '我是一条测试的描述',
+  },
+  {
+    id: '4',
+    name: 'TechUI',
+    image:
+      'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+    desc: '我是一条测试的描述',
+  },
+];
+
 const Settings = () => {
+  const [dataSource, setDataSource] = useState(defaultData);
+
   return (
     <>
       <PageContainer title="个人设置">
@@ -14,14 +65,64 @@ const Settings = () => {
         <ProCard style={{ marginTop: 24 }} ghost gutter={(0, 8)}>
           <ProCard ghost gutter={(0, 8)} direction="column">
             <ProCard title="账号注销" headerBordered>
-              1
+              <Alert
+                message="您可以在此注销当前账号。账号注销成功后,当前账号内的所有服务将不可用,除法律法规另有规定外,当前账号内信息、数据将被删除，且无法恢复。"
+                type="error"
+              />
+              <Button className="mt-5" type="primary" danger onClick={handleDelete}>
+                我要注销！！！
+              </Button>
             </ProCard>
             <ProCard title="其他联系方式" headerBordered style={{ marginTop: 24 }}>
-              2
+              你还有其他方式???
             </ProCard>
           </ProCard>
-          <ProCard title="安全信息" headerBordered h>
-            绑定信息
+          <ProCard title="安全信息" headerBordered>
+            <ProList
+              rowKey="id"
+              dataSource={dataSource}
+              editable={{
+                onSave: async (key, record, originRow) => {
+                  console.log(key, record, originRow);
+                  return true;
+                },
+              }}
+              onDataSourceChange={setDataSource}
+              metas={{
+                title: {
+                  dataIndex: 'name',
+                },
+                avatar: {
+                  dataIndex: 'image',
+                  editable: false,
+                },
+                description: {
+                  dataIndex: 'desc',
+                },
+                subTitle: {
+                  render: () => {
+                    return (
+                      <Space size={0}>
+                        <Tag color="blue">Ant Design</Tag>
+                        <Tag color="#5BD8A6">TechUI</Tag>
+                      </Space>
+                    );
+                  },
+                },
+                actions: {
+                  render: (text, row, index, action) => [
+                    <a
+                      onClick={() => {
+                        action?.startEditable(row.id);
+                      }}
+                      key="link"
+                    >
+                      编辑
+                    </a>,
+                  ],
+                },
+              }}
+            />
           </ProCard>
         </ProCard>
       </PageContainer>
