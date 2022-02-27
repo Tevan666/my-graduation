fc4017c7be2a41e63a1f2cf98d344f8bd59bebf5
Tevan <1759377257@qq.com>
2022-02-27 21:02:31 +0800

    底部加一个可爱的小熊快跑

---
+++ src/components/BearRun/index.jsx
@@ -0,0 +1,13 @@
+import style from './index.less';
+
+const BearRun = () => {
+  return (
+    <>
+      <div className={style.mountain}>
+        <div className={style.bear} />
+      </div>
+    </>
+  );
+};
+
+export default BearRun;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,7 @@ import { useIntl } from 'umi';
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
 
+import BearRun from '../BearRun/index.jsx';
 const Footer = () => {
   const intl = useIntl();
   const defaultMessage = intl.formatMessage({
@@ -10,29 +11,34 @@ const Footer = () => {
   });
   const currentYear = new Date().getFullYear();
   return (
-    <DefaultFooter
-      copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Tevan666',
-          href: 'https://tevan666.github.io/vuepress/',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
-    />
+    <>
+      <BearRun className="max-h-100" />
+      <DefaultFooter
+        className="z-10 absolute left-1/2 bottom-0 -translate-x-1/2 bg-transparent"
+        style={{ transform: 'translate(-50%,0)' }}
+        copyright={`${currentYear} ${defaultMessage}`}
+        links={[
+          {
+            key: 'Ant Design Pro',
+            title: 'Tevan666',
+            href: 'https://tevan666.github.io/vuepress/',
+            blankTarget: true,
+          },
+          {
+            key: 'github',
+            title: <GithubOutlined />,
+            href: 'https://github.com/ant-design/ant-design-pro',
+            blankTarget: true,
+          },
+          {
+            key: 'Ant Design',
+            title: 'Ant Design',
+            href: 'https://ant.design',
+            blankTarget: true,
+          },
+        ]}
+      />
+    </>
   );
 };
 
